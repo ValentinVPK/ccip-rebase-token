cf6b892b55b81065d2ecc25017945960ffd6e062
ValentinVPK <valentinvpk@abv.bg>
2025-04-03 08:37:47 +0300

    Creating and writing the rebase token contract

---
+++ src/RebaseToken.sol
@@ -0,0 +1,148 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+/**
+ * @title RebaseToken
+ * @author Valentin Krumov
+ * @notice This is a cross-chain rebase token that incentivises users to deposit into a vault and gain interest in rewards
+ * @notice The interest rate in the smart contract can only decrease
+ * @notice Each user will have their own interest rate that is the global interest rate at the time of depositing
+ */
+contract RebaseToken is ERC20 {
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    error RebaseToken__InterestRateCanOnlyDecrease();
+
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+    uint256 private constant PRECISION_FACTOR = 1e18;
+    uint256 private s_interestRate = 5e10;
+    mapping(address user => uint256 interestRate) private s_userInterestRate;
+    mapping(address user => uint256 lastUpdatedTimestamp) private s_userLastUpdatedTimestamp;
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    event InterestRateSet(uint256 indexed newInterestRate);
+
+    constructor() ERC20("RebaseToken", "RBT") {}
+
+    /*//////////////////////////////////////////////////////////////
+                    EXTERNAL & PUBLIC FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Set the interest rate in the contract
+     * @param _newInterestRate The new interest rate to set
+     * @dev The interest rate can only decrease
+     */
+    function setInterestRate(uint256 _newInterestRate) external {
+        // ????
+        if (_newInterestRate > s_interestRate) {
+            revert RebaseToken__InterestRateCanOnlyDecrease();
+        }
+
+        s_interestRate = _newInterestRate;
+        emit InterestRateSet(_newInterestRate);
+    }
+
+    /**
+     * @notice Mint the user tokens when they deposit into the vault
+     * @param _to The user to mint the tokens to
+     * @param _amount The amount of tokens to mint
+     */
+    function mint(address _to, uint256 _amount) external {
+        _mintAccruedInterest(_to);
+        s_userInterestRate[_to] = s_interestRate;
+        _mint(_to, _amount);
+    }
+
+    /**
+     * @notice Calculate the balance of the user including any accumulated interest since the last update
+     * (principle balance) + some interest that has accured
+     * @param _user The user to calculate the balance of
+     * @return The balance of the user including any accumulated interest
+     */
+    function balanceOf(address _user) public view override returns (uint256) {
+        // get the current principle balance of the user (the number of tokens that have actually been minted to the user)
+        // multiply the principle balance by the interest that has accumulated in the time since the balance was last updated
+        return (super.balanceOf(_user) * _calculateUserAccumulatedInterestSinceLastUpdate(_user)) / PRECISION_FACTOR;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                      INTERNAL & PRIVATE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Calculate the interest that has accumulated since the last update
+     * @param _user The user to calculate the accumulated interest for
+     * @return linearInterest The interest that has accumulated since the last update
+     */
+    function _calculateUserAccumulatedInterestSinceLastUpdate(address _user)
+        internal
+        view
+        returns (uint256 linearInterest)
+    {
+        // we need to calculate the interest that has accumulated since the last update
+        // This is going to be linear growth with time
+        // 1. Calculate the time since the last update
+        // 2. Calculate the amount of linear growth
+        // (principal amount) + (principal amount * interest rate * time since last update)
+        // deposit: 10 tokens
+        // interest rate: 0.5 tokens per second
+        // time elapsed is 2 seconds
+        // 10 + (10 * 0.5 * 2) = 20 tokens
+
+        uint256 timeElapsed = block.timestamp - s_userLastUpdatedTimestamp[_user];
+        linearInterest = PRECISION_FACTOR + (s_interestRate * timeElapsed);
+    }
+
+    function _mintAccruedInterest(address _user) internal {
+        // (1) find their current balance of rebase tokens that have been minted to the user -> principalBalance
+        // (2) calculate their current balance including any interest -> balanceOf
+        // (3) calculate the number of tokens that need to be minted to the user -> (2) - (1)
+        // call _mint to mint the tokens to the user
+        // set the user's last updated timestamp
+        s_userLastUpdatedTimestamp[_user] = block.timestamp;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                         VIEW & PURE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Get the interest rate for a user
+     * @param _user The address of the user
+     * @return The interest rate for the user
+     */
+    function getUserInterestRate(address _user) external view returns (uint256) {
+        return s_userInterestRate[_user];
+    }
+}
